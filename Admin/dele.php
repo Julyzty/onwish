f19b9438318bcc32ff18cccf0cf099907ae8ecd0
zhangtengyu <zhangtengyu@xiaobu121.com>
2020-06-11 12:00:55 +0800

    first

--- Admin/dele.php
+++ Admin/dele.php
@@ -2,7 +2,7 @@
       header('content-type:text/html;charset=utf-8');
       //接收id值 连接数据库 SQL语句 执行
       $id=$_GET['id'];
-       
+
       $link=mysqli_connect('127.0.0.1','root','root','wishwall') or exit('连接失败');
       $sql="delete from wish where id=$id";
       $res=mysqli_query($link,$sql);
@@ -10,7 +10,7 @@
         echo '删除成功';
         header("refresh:2;url='list.php'");
     }else{
-        echo '删除失败';
+        echo '删除失败111';
         header("refresh:2;url='list.php'");
     }
 ?>
